0cbe53cbdec0250102f394b0e17c171faf87d1dd
Ismaël SAÏ <ismael.sai@etu.umontpellier.fr>
2019-11-12 15:25:03 +0100

    init squeleton

---
+++ inc/client.h
@@ -0,0 +1,22 @@
+#ifndef __CLIENT_H
+#define __CLIENT_H
+
+/* includes */
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <unistd.h>
+#include <time.h>
+/* end of includes */
+
+
+
+
+
+#endif
---
+++ inc/server.h
@@ -0,0 +1,22 @@
+#ifndef __SERVER_H
+#define __SERVER_H
+
+/* includes */
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <unistd.h>
+#include <time.h>
+/* end of includes */
+
+
+
+
+
+#endif
